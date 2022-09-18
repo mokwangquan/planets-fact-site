27eaf2b76c4d501a5ea8feccf30e1f0ef7c492bf
Mok Wang Quan <mok.wq@chimaera.my>
2022-09-18 13:05:11 +0800

    done font init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from "@/components/header/index.vue"
+
 export default {
+  name: "App",
+  components: {
+    Header
+  },
   computed: {
     
   },
@@ -19,4 +26,10 @@ export default {
 </script>
 
 <style lang="scss">
+// import third party first
+@import "https://unpkg.com/element-ui/lib/theme-chalk/index.css";
+@import "@/assets/style/element-variables.scss";
+
+// Then only import local, to allow overriding
+@import "@/assets/style/main.scss";
 </style>
---
+++ src/components/header/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="header">
+    <el-row type="flex" justify="space-around" align="middle">
+      <el-col>
+        <h1>THE PLANETS</h1>
+        <h2>THE PLANETS</h2>
+        <h3>THE PLANETS</h3>
+        <h4>THE PLANETS</h4>
+      </el-col>
+      <el-col>
+        Lorem ipsum
+      </el-col>
+    </el-row>
+  </div>
+</template>
